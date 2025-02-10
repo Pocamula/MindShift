333428654aad685296bfc6c8e2955d8c1b96e584
Pocamula <pocamularadio@gmail.com>
2025-02-10 00:00:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MindShift/',  // Asegura rutas correctas en GitHub Pages
   plugins: [react()],
 })
