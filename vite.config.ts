1accb81b4d2a9e5925642f1995f504d619869afb
Pocamula <pocamularadio@gmail.com>
2025-02-10 19:24:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  server: {
-    port: 5173, // Puedes cambiar el puerto si es necesario
-  },
+  plugins: [react()],
 });
